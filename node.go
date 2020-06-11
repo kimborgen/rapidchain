04d931e3743d83126ff765e1d3bd97e6a509774d
Kim Borgen <me@kimborgen.com>
2020-06-12 01:34:46 +0200

    move global vars to local, clean and prime node code for next work

--- node.go
+++ node.go
@@ -1,34 +1,44 @@
 package main
 
 import (
+	"fmt"
 	"log"
 	"math/rand"
 	"net"
 )
 
+// Representation of a member beloning to the current committee of a node
 type CommitteeMember struct {
 	ID uint
-	IP uint
+	IP string
 }
 
+// Representation of a committee from the point of view of a node
 type Committee struct {
 	ID      uint
-	members []CommitteeMember
+	Members map[uint]CommitteeMember
 }
 
-var nodesAllInfo []NodeAllInfo
-var selfAllInfo NodeAllInfo
-var initialRandomness int
-var currentCommittee Committee
-
-// information about yourself
+// Cheat data to know all nodes in the system
+type AllInfo struct {
+	self  NodeAllInfo
+	nodes map[uint]NodeAllInfo
+}
 
 func launchNode() {
 	conn := dial("127.0.0.1:8080")
-	coordinatorSetup(conn)
+	allInfo, initialRandomness, currentCommittee := coordinatorSetup(conn)
+	fmt.Print(allInfo, initialRandomness, currentCommittee, "\n\n")
+
+	// launch listener
+	go listen()
+
+	// blocker
+	for {
+	}
 }
 
-func coordinatorSetup(conn net.Conn) {
+func coordinatorSetup(conn net.Conn) (AllInfo, int, Committee) {
 	// setup with the help of coordinator
 
 	// create a random ID
@@ -41,26 +51,51 @@ func coordinatorSetup(conn net.Conn) {
 	response := new(ResponseToNodes)
 	reciveMsg(conn, response)
 
-	nodesAllInfo = response.Nodes
+	// declare variables to return
+	var allInfo AllInfo
+	var initialRandomness int
+	var currentCommittee Committee
+
+	allInfo.nodes = make(map[uint]NodeAllInfo)
+	for _, elem := range response.Nodes {
+		allInfo.nodes[elem.ID] = elem
+	}
+	allInfo.self = allInfo.nodes[ID]
+
 	initialRandomness = response.InitalRandomness
 
-	// go trough the list and find yourself
-	// also double check if there is another node with same id
-	// TODO: remove double check when pk scheme is implemented
-	found := false
+	// create current commitee info
+	currentCommittee.ID = allInfo.self.CommitteeID
+	currentCommittee.Members = make(map[uint]CommitteeMember) // initialize map
 
-	/*
-		for _, elem := range nodesAllInfo {
-			if elem.ID == ID {
-				if !found {
-					selfAllInfo = elem
-					found = true
-				} else {
-					log.Fatal("Found other node with same id")
-				}
-			}
+	for k, v := range allInfo.nodes {
+		if v.CommitteeID == currentCommittee.ID && k != ID {
+			fmt.Println("\nk: ", k, " v: ", v)
+			currentCommittee.Members[k] = CommitteeMember{v.ID, v.IP}
 		}
-	*/
+	}
 
 	log.Printf("Coordinaton setup finished \n")
+	return allInfo, initialRandomness, currentCommittee
+}
+
+func listen() {
+	listener, err := net.Listen("tcp", ":8080")
+	ifErrFatal(err, "tcp listen on port 8080")
+
+	// block main and listen to all incoming connections
+	for {
+
+		// accept new connection
+		conn, err := listener.Accept()
+		ifErrFatal(err, "tcp accept")
+
+		go nodeHandleConnection(conn)
+		// spawn off goroutine to able to accept new connections
+		//go coordinatorHandleConnection(conn, chanToCoordinator, chanToNodes[i], &wg, &wg_done)
+	}
+}
+
+func nodeHandleConnection(conn net.Conn) {
+
 }
