42a3dd7f985049f53707ab9e85f95aa124a8b9b0
Kim Borgen <me@kimborgen.com>
2020-06-14 19:42:18 +0200

    add a random sampling without replacment algo and add/change err funcs

--- helpers.go
+++ helpers.go
@@ -1,15 +1,59 @@
 package main
 
-import "log"
+import (
+	"log"
+	"math/rand"
+)
 
-func ifErr(e interface{}, msg string) {
+func ifErr(e interface{}, msg string) bool {
 	if e != nil {
 		log.Printf("[Error] %s with error %s", msg, e)
+		return true
 	}
+	return false
 }
 
-func ifErrFatal(e interface{}, msg string) {
+func ifErrFatal(e interface{}, msg string) bool {
 	if e != nil {
-		log.Fatalf("[Fatal] %s with error: %s", msg, e)
+		log.Fatalf("[Fatal] msg(%s) with error(%s)", msg, e)
+		panic(e)
+		return true
 	}
+	return false
+}
+
+func errr(e interface{}, msg string) {
+	log.Printf("[Error] %s with error %s", msg, e)
+}
+
+func errFatal(e interface{}, msg string) {
+	log.Fatalf("[Fatal] %s with error: %s", msg, e)
+	panic(e)
+}
+
+func randIndexesWithoutReplacement(arrayLength, sampleSize int) []int {
+	// Sample a list of sampleSize random indexes in an array of length arrayLength
+	// record indexes here to prevent duplicates
+	indexes := make(map[int]bool)
+
+	// create n random indexes
+	for i := 0; i < sampleSize; i++ {
+		var r int
+		for {
+			r = int(rand.Int63n(int64(arrayLength)))
+			if indexes[r] {
+				continue
+			}
+			break
+		}
+
+		indexes[r] = true
+	}
+
+	keys := make([]int, 0, len(indexes))
+	for k := range indexes {
+		keys = append(keys, k)
+	}
+
+	return keys
 }
