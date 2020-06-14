b1d4e60ef918eb3f3b120a8e11a1dd350b4754f0
Kim Borgen <me@kimborgen.com>
2020-06-14 19:40:44 +0200

    Refactor flags into single struct

--- defaults.go
+++ defaults.go
@@ -1,11 +1,33 @@
 package main
 
+import "math"
+
 // Default variables to be used if flag is not set
 
-const default_func string = "node"
+const default_function string = "node"
 const default_vCPUs uint = 1
-const default_instances uint = 19
-const default_n uint = 19
-const default_m uint = 2
+const default_instances uint = 10
+const default_n uint = 10
+const default_m uint = 1
 const default_totalF uint = 3
 const default_committeeF uint = 2
+const default_d uint = 3
+
+var default_B uint = uint(math.Pow(2, 21)) // 2 mill
+
+// TODO add these to flags and so on
+const default_kappa = 128 //128
+const default_phi = 0.63
+const default_parity = 80 // 80?
+
+type FlagArgs struct {
+	function   string
+	vCPUs      uint
+	instances  uint
+	n          uint
+	m          uint
+	totalF     uint
+	committeeF uint
+	d          uint
+	B          uint
+}
--- main.go
+++ main.go
@@ -1,37 +1,53 @@
 package main
 
 import (
+	"encoding/gob"
 	"flag"
-	"fmt"
 )
 
 func main() {
 	// Program starts here. This function will spawn the x RC instances.
 
 	// defaults in defaults.go
-	funcPtr := flag.String("func", default_func, "coordinator or node")
+	functionPtr := flag.String("function", default_function, "coordinator or node")
 	vCPUs := flag.Uint("vpcus", default_vCPUs, "amount of VCPUs available")
 	instancesPerVCPUPtr := flag.Uint("instances", default_instances, "Instances per VCPU")
 	nPtr := flag.Uint("n", default_n, "Total amount of nodes")
 	mPtr := flag.Uint("m", default_m, "Number of committees")
 	totalFPtr := flag.Uint("totalF", default_totalF, "Total adversary tolerance in the form of the divisor (1/x)")
 	committeeFPtr := flag.Uint("committeeF", default_committeeF, "Committee adversary tolerance in the form of the divisor (1/x)")
-
-	fmt.Printf("%d", maxId)
+	dPtr := flag.Uint("d", default_d, "d neighbours")
+	BPtr := flag.Uint("B", default_B, "block size in bytes")
 
 	flag.Parse()
 
-	if *funcPtr == "coordinator" {
+	var flagArgs FlagArgs
+
+	flagArgs.function = *functionPtr
+	flagArgs.vCPUs = *vCPUs
+	flagArgs.instances = *instancesPerVCPUPtr
+	flagArgs.n = *nPtr
+	flagArgs.m = *mPtr
+	flagArgs.totalF = *totalFPtr
+	flagArgs.committeeF = *committeeFPtr
+	flagArgs.d = *dPtr
+	flagArgs.B = *BPtr
+
+	// register structs with gob
+	gob.Register(IDAGossipMsg{})
+	gob.Register([32]uint8{})
+
+	if *functionPtr == "coordinator" {
 		launchCoordinator(*nPtr, *mPtr, *totalFPtr, *committeeFPtr)
 	} else {
-		launchNodes(*vCPUs, *instancesPerVCPUPtr)
+		launchNodes(&flagArgs)
 	}
 
 }
 
-func launchNodes(vCPUs, instancesPerVCPU uint) {
-	for i := uint(0); i < vCPUs*instancesPerVCPU; i++ {
-		go launchNode()
+func launchNodes(flagArgs *FlagArgs) {
+	for i := uint(0); i < flagArgs.vCPUs*flagArgs.instances; i++ {
+		go launchNode(flagArgs)
 	}
 
 	for {
