abeb8c6a1fbc86d8adb8b34b09e7f22e5dd1762d
Kim Borgen <me@kimborgen.com>
2020-07-13 02:38:55 +0200

    Add debug logging

--- aws-startup-script.sh
+++ aws-startup-script.sh
@@ -18,10 +18,11 @@ Content-Transfer-Encoding: 7bit
 Content-Disposition: attachment; filename="userdata.txt"
 
 #!/bin/bash -x
-wget -O /home/ubuntu/rapidchain https://rapidchain-bucket.s3.amazonaws.com/rapidchain > /tmp/wget.log
-chmod +x /home/ubuntu/rapidchain > /tmp/chmod.log
+exec > /tmp/part-001.log 2>&1
+wget -O /home/ubuntu/rapidchain https://rapidchain-bucket.s3.amazonaws.com/rapidchain
+chmod +x /home/ubuntu/rapidchain
 ulimit -n 65000
 sysctl -w net.ipv4.tcp_tw_reuse=1
-/home2/ubuntu/rapidchain > /tmp/rapidchain.log
+/home/ubuntu/rapidchain
 
 --//
